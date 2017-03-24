56b85e91b67e1e5fcead969502a8984bb28b06a8
Paul Chung <paulchung39@gmail.com>
2017-03-23 21:55:45 -0700

    implemented categories with category chips

--- src/components/new_entry.js
+++ src/components/new_entry.js
@@ -7,6 +7,8 @@ import MenuItem from 'material-ui/MenuItem';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import RaisedButton from 'material-ui/RaisedButton';
 import Chip from 'material-ui/Chip';
+import Avatar from 'material-ui/Avatar';
+import {blue300, indigo900} from 'material-ui/styles/colors';
 
 injectTapEventPlugin();
 
@@ -47,24 +49,26 @@ class NewEntry extends Component {
     value: 1,
   };
 
-  handleChange = (event, index, value) => {
-    console.log(value);
+  appendCategory = (event, index, value) => {
     var categoriesList = this.state.categories;
-    this.setState({ categories: [ ...categoriesList, {value} ] });
-    this.handleCategories();
+    this.setState({ categories: [ ...categoriesList, {"id": ++this.state.categories.length, "value": {value}} ]});
   };
 
-  handleCategories = () => {
-    console.log(this.state.categories);
-  };
+  handleRequestDelete = (key_id) => {
+    var categoriesList = this.state.categories;
+    const indexItem = categoriesList.indexOf(key_id);
+    categoriesList.splice(indexItem, 1);
+    this.setState({categories: [ ...categoriesList ]});
+  }
 
   renderChip(data) {
     return (
       <Chip
         style={this.styles.chip}
-        key={this.state.categories.indexOf(data)}
-      >
-        {data.value}
+        key={data.id}
+        onRequestDelete={() => this.handleRequestDelete(data.id)} >
+        <Avatar size={32} color={blue300} backgroundColor={indigo900} >A</Avatar>
+        {data.value.value}
       </Chip>
     );
   }
@@ -100,6 +104,20 @@ class NewEntry extends Component {
             onChange={this.handleChange}>
             <MenuItem value="Accepted" primaryText="Accepted" />
             <MenuItem value="Rejected" primaryText="Rejected" />
+          </SelectField>
+          </div>
+
+          <div className="categories">
+          <SelectField
+            {...status}
+            floatingLabelText="Choose Categories"
+            floatingLabelFixed={true}
+            value={this.state.value}
+            onChange={this.appendCategory}>
+            <MenuItem value="Romance" primaryText="Romance" />
+            <MenuItem value="Family" primaryText="Family" />
+            <MenuItem value="Friends" primaryText="Friends" />
+            <MenuItem value="Career" primaryText="Career" />
           </SelectField>
           </div>
 
